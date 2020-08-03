ca25e4f46ce9de095fbbd630e9628366a926cb8c
Mads <51197902+mkingsley203@users.noreply.github.com>
2020-08-03 11:48:27 +1000

    Update gatsby-config.js

--- THU_Web/gatsby-config.js
+++ THU_Web/gatsby-config.js
@@ -27,6 +27,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        baseUrl: `http://www.admin.trueheart.com.au/`,
+        apiBase: `api`, // optional, defaults to `jsonapi`
+        //can add basic auth
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
