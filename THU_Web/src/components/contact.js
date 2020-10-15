cb850257a7c0caade2a9c6e681b7990800047c75
mkingsley203 <51197902+mkingsley203@users.noreply.github.com>
2020-10-15 15:36:58 +1000

    forms updates

--- THU_Web/src/components/contact.js
+++ THU_Web/src/components/contact.js
@@ -2,11 +2,11 @@ import React from "react";
 
 import '../pages/main.css';
 
-export default function Contact(){
+export default function Contact(name){
   return (
     <div class="half-width" id="message">
         <h2>Send Us a Message</h2>
-        <form id="contactform" action="https://formsubmit.co/testingtruweb@gmail.com" method="POST">
+        <form name={name} method="POST" data-netlify="true">
             <div class="flex space_between">
                 <div class="form-half-width">
                     <p>Your Name<span class="red">*</span></p>
