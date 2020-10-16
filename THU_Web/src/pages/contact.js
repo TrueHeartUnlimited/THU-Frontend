000c4714220753c48fa34d53379eb927e3e12426
mkingsley203 <51197902+mkingsley203@users.noreply.github.com>
2020-10-16 11:59:03 +1000

    Revert "if this doesn't work then it's not a me issue"
    This reverts commit 8006d9e485619bc316b40900eaed432f59592568.

--- THU_Web/src/pages/contact.js
+++ THU_Web/src/pages/contact.js
@@ -1,80 +1,109 @@
-import React from "react";
-import { navigateTo } from "gatsby-link";
+import React, { useState } from "react"
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"
 
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-}
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import ContactForm from "../components/contact"
+import StyledBackgroundSection from '../components/BackgroundSection'
 
-export default class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+const containerStyle = {
+  width: '100%',
+  height: '100%'
+};
+
+const center = {
+  lat: -27.464320,
+  lng: 153.011270
+};
+
+const SecondPage = () =>{ 
+  const [formState, setFormState] = useState({
+    name:"",
+    email:"",
+    message:""
+  })
+
+  const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
   }
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+  const handleSubmit = e => {
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state
-      })
+      body: encode({ "form-name": "contact", ...formState })
     })
-      .then(() => navigateTo(form.getAttribute("action")))
+    // this will become navigate
+      .then(() => alert("Success!"))
       .catch(error => alert(error));
+
+      e.preventDefault();
   };
 
-  render() {
-    return (
-      <div>
-        <h1>Contact</h1>
-        <form
-          name="contact"
-          method="post"
-          action="/thanks/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out:{" "}
-              <input name="bot-field" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your name:<br />
-              <input type="text" name="name" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your email:<br />
-              <input type="email" name="email" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:<br />
-              <textarea name="message" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      </div>
-    );
+  const handleChange = e => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value
+    })
   }
+
+  return(
+    <Layout>
+      <SEO title="Contact Us" />
+          <StyledBackgroundSection class="header">
+              <h1>Contact Us</h1>
+          </StyledBackgroundSection>
+      <div class="container" id="contact-us">
+          <div>
+              <p>If you have any queries relating to our products, processes or any concerns please feel free to contact us. Send us a message below or come visit us at our base of operations. Otherwise feel free to call or send an email, our details are listed at the bottom of the page.</p>
+          </div>
+          {/* Form */}
+          <div class="flex separator space_between">
+          <div class="half-width" id="message">
+            <h2>Send Us a Message</h2>
+            <form name="contact" method="post" data-netlify="true" onSubmit={handleSubmit}>
+              <input type="hidden" name="form-name" value="contact"/>
+                <div >
+                    <div >
+                        <label htmlFor="name">Your Name<span >*</span>
+                        <input type="text" name="name"  value={formState.name} onChange={handleChange}/></label>
+                    </div>
+                    <div >
+                        <label htmlFor="email">Your Email<span >*</span>
+                        <input type="email" name="email"  value={formState.email} onChange={handleChange}/></label>
+                    </div>
+                </div>
+                <label htmlFor="message">Message<span >*</span>
+                <textarea name="message" value={formState.message} onChange={handleChange}></textarea></label>
+                <button type="submit">Submit</button>
+              </form>          
+            </div>
+              <div class="half-width">
+                  <div class="location-info">
+                      <h2>Our Location</h2>
+                      <div id="map">
+                          <LoadScript googleMapsApiKey="AIzaSyBaLBI6hzrNKKCL5wFZbYj1LN13PJK7RV4">
+                              <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+                                  { /* Child components, such as markers, info windows, etc. */ }
+                                  <Marker position={center}/>
+                              </GoogleMap>
+                          </LoadScript>
+                      </div>
+                  </div>
+                  <div class="separator location-info">
+                      <h2>Business Hours</h2>
+                      <p>Monday - Friday: 9:00am - 5:00pm</p>
+                      <p>Saturday & Sunday: Closed</p>
+                      <p>Public Holidays: Closed</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </Layout>
+  )
 }
+
+export default SecondPage
